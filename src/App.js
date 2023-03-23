a3c49bb6229c51a6359d79dffc40c95e7dece6ee
Jarmel <pawel.jarmula@gmail.com>
2023-03-23 20:58:08 +0100

    inputs added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
+
 import Expense from "./components/expenses/Expense";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expense />
     </div>
   );
